02e1bd3b640566345f860dde8aca0a0ff6165e0f
MartinaEO <campese.martina@gmail.com>
2021-01-24 20:40:31 +0100

    Un po' di gerarchia

---
+++ flat.h
@@ -0,0 +1,25 @@
+#ifndef FLAT_H
+#define FLAT_H
+#include<vacation.h>
+
+class Flat: public Vacation {
+private:
+    unsigned int rooms;
+    bool AC;
+public:
+    Flat(string="", string="", string="", int=2020, int=1, int=1, double=0.0, unsigned int=1 , unsigned int=1, bool=false);
+    Flat* clone() override;
+    double calcFinalPrice() const override;
+    double calcCommission() const override;
+    //void print() const override();
+
+
+    bool hasAC() const;
+    unsigned int getRooms() const;
+
+    void setRooms(int ro);
+    void setAC(bool ac);
+
+};
+
+#endif // FLAT_H
---
+++ vacation.h
@@ -0,0 +1,50 @@
+#ifndef VACATION_H
+#define VACATION_H
+#include<QDate>
+using std::string;
+
+class Vacation
+{
+private:
+    string name;
+    string place;
+    string country;
+    QDate date;
+    double basePrice;
+    unsigned int weeks;
+
+public:
+    Vacation(string="", string="", string="", int=2020, int=1, int=1, double=0.0, unsigned int=1 );
+    virtual ~Vacation() =default;
+
+
+
+    //metodi virtuali
+    virtual Vacation* clone() const=0;
+    virtual double calcFinalPrice() const=0;
+    //virtual print() const;
+    virtual double calcCommission() const=0;
+    virtual bool operator==(const Vacation&) const;//utile? da implementare?
+
+
+
+    string getName() const;
+    string getPlace() const;
+    string getCountry() const;
+    QDate getDate() const;
+    int getYearD() const;
+    int getMonthD() const;
+    int getDayD() const;
+    double getBasePrice() const;
+    unsigned int getWeeks() const;
+
+
+    void setName(string n);
+    void setPlace(string p);
+    void setCountry(string c);
+    bool setDateVacation(int y, int m, int d);
+    void setBasePrice(double bP);
+    void setWeeks(int w);
+};
+
+#endif // VACATION_H
